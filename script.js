449c46be8b088f81b6b77ffd503345b95f96f14c
SSSergiy <sergey1991serg@gmail.com>
2022-06-25 23:46:49 +0300

    bogdan ymene polychulos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
-console.log('Nekritov')
-console.log('Nekrit')
-console.log('Nekri')
+document.body.innerHTML = '<h1>богдан у мене получилось</h1>'
+console.log('богдан у мене получилось')
+console.log('богдан у мене получилось')
+console.log('богдан у мене получилось')
